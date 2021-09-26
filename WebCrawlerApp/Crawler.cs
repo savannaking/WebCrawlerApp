ef2f65792b6245383530daac25ce474c242472e4
Savanna King <savanna.king@medicalsolutions.com>
2021-09-26 14:34:30 -0500

    Add project files.

---
+++ WebCrawlerApp/Crawler.cs
@@ -0,0 +1,115 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebCrawlerApp
+{
+    public class Crawler
+    {
+        private Queue<string> linksToTraverse;
+        private List<string> linksTraversed;
+        private List<string> phoneNumbers;
+        private string phoneRegex = @"\d{3}[\s.-]\d{3}[\s.-]\d{4}";
+        private int crawlDepth;
+        private int crawlDepthCounter;
+        private bool stayWithinDomain;
+
+        public Crawler(int crawlDepth, bool stayWithinDomain = false)
+        {
+            linksTraversed = new List<string>();
+            linksToTraverse = new Queue<string>();
+            phoneNumbers = new List<string>();
+            this.crawlDepth = crawlDepth;
+        }
+
+        public void Crawl(string startingUrl)
+        {
+            crawlDepthCounter++;
+            if (crawlDepthCounter > crawlDepth)
+                return;
+
+            var uri = new Uri(startingUrl);
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(startingUrl);
+
+            ScrapeLinks(htmlDoc, startingUrl, stayWithinDomain);
+
+            ScrapePhoneNumbers(htmlDoc);
+
+            linksTraversed.Add(startingUrl);
+            if (linksToTraverse.Count > 0)
+            {
+                Crawl(linksToTraverse.Dequeue());
+            }
+        }
+
+        private void ScrapePhoneNumbers(HtmlDocument htmlDoc)
+        {
+            foreach (var node in htmlDoc.DocumentNode.SelectNodes("//text()[normalize-space(.) != '']"))
+            {
+                Match m = Regex.Match(node.InnerText, phoneRegex);
+                if (m.Success && !phoneNumbers.Contains(m.Value))
+                {
+                    phoneNumbers.Add(m.Value);
+                    Console.WriteLine(m.Value);
+                }
+            }
+        }
+
+        private void ScrapeLinks(HtmlDocument htmlDoc, string startingUrl, bool stayWithinDomain)
+        {
+            var links = htmlDoc.DocumentNode.SelectNodes("//a[@href]");
+            if (links != null)
+            {
+                foreach (HtmlNode link in links)
+                {
+                    var url = link.GetAttributeValue("href", null);
+                    url = GetAbsoluteUrlFromScrapedLink(url, startingUrl);
+                    var startingUri = new Uri(startingUrl);
+                    var uri = new Uri(url);
+
+                    if (stayWithinDomain)
+                    {
+                        if (uri.Host != startingUri.Host)
+                        {
+                            continue;
+                        }
+                    }
+                    if (url != null && !linksTraversed.Contains(url) && !linksToTraverse.Contains(url))
+                        linksToTraverse.Enqueue(url);
+                }
+            }
+        }
+
+        private string GetAbsoluteUrlFromScrapedLink(string url, string startingUrl)
+        {
+            Uri inputUri = new Uri(startingUrl);
+
+            //if it's already an absolute URL, just add it to the queue
+            Uri result;
+            if (Uri.TryCreate(url, UriKind.Absolute, out result))
+            {
+                return url;
+            }
+            else
+            {
+                if (url.StartsWith("/"))
+                    return inputUri.Scheme + @"://" + inputUri.Host + url;
+                else
+                {
+
+                    var fullUrl = String.Format(
+                        "{0}{1}{2}",
+                        inputUri.GetLeftPart(UriPartial.Authority),
+                        String.Join(string.Empty, inputUri.Segments.Take(inputUri.Segments.Length - 1)), url);
+                    return fullUrl;
+                }
+            }
+        }
+    }
+}
---
+++ WebCrawlerApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebCrawlerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a starting URL.");
+            string url = Console.ReadLine();
+            Console.WriteLine("Enter a crawl depth as an integer.");
+            int crawlDepth = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Do you want to stay within the domain of your starting URL? Type Y/N");
+            bool stayWithinDomain = Console.ReadLine().ToUpper() == "Y" ? true : false;
+
+            var crawler = new Crawler(crawlDepth, stayWithinDomain);
+            crawler.Crawl(url);
+        }
+    }
+}
